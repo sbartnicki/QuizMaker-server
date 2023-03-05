42c08ea29209c8340815fb7550b7cfa49d5830f3
sbartnicki <53477860+sbartnicki@users.noreply.github.com>
2023-03-05 03:29:27 -0500

    Added user validation at login

--- routes/users.js
+++ routes/users.js
@@ -17,6 +17,9 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = await User.findOne({ email: req.body.email });
   if (!user)
     return res.status(400).send("User with given e-mail doesn't exist");
